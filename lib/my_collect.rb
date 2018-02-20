592c96f7173e3bc3a07deaf360395835495987f7
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-20 19:37:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,4 @@
 def my_collect(collection)
-  if block_given?
   i = 0
   new_collection = []
   while i < collection.length
@@ -7,5 +6,4 @@ def my_collect(collection)
     i += 1
   end
   new_collection
-end
 end
